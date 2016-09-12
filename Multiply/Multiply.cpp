125a81747908eb00309f9d9fe3e7144e1d161a75
Harry Kalra <kalra_harry@hotmail.com>
2016-09-11 20:46:24 -0400

    Multiplication Table

--- Multiply/Multiply.cpp
+++ Multiply/Multiply.cpp
@@ -2,10 +2,27 @@
 //
 
 #include "stdafx.h"
+#include <iostream>
 
+using namespace std;
 
 int main()
 {
-    return 0;
+
+	cout << "Multiplication tables: " << endl;
+	cout << "0 \t1 \t2 \t3 \t4 \t5 \t6 \t7 \t8 \t9 \t10 " << endl;
+	for (int i = 1; i<10; i++)
+	{
+		cout << i << '\t';
+		for (int a=1; a<=10; a++)
+		{
+			cout << a*i << '\t';
+			
+		}
+		cout << endl;
+	}
+
+	return 0;
+	
 }
 
